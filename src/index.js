1d8156dbb1207c80d43aac0ccc24570a4f72d97d
Argenis Osorio <argenisosorio580@hotmail.com>
2021-05-15 23:50:24 -0400

    Added Table component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from "./App";
+import { Table } from "./Table";
 import './main.css';
 
 ReactDOM.render(< App />, document.getElementById('root'));
+ReactDOM.render(< Table />, document.getElementById('table'));
