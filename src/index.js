3b06584ee99a3f173fedc9fc35533879b1d0e7dc
Argenis Osorio <argenisosorio580@hotmail.com>
2021-05-15 23:57:37 -0400

    Added new component and moved files to new directory

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from "./App";
-import { Table } from "./Table";
-import './main.css';
+import { App } from "./components/App";
+import { Table } from "./components/Table";
+import './css/main.css';
 
 ReactDOM.render(< App />, document.getElementById('root'));
 ReactDOM.render(< Table />, document.getElementById('table'));
